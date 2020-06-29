5cefa780c139835f47a962b8a04cbe999eb367a2
ebeagusamuel <samuelebeagu@gmail.com>
2020-06-29 14:30:55 +0100

    add model, controller and view for the posts

--- Gemfile
+++ Gemfile
@@ -53,3 +53,4 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rubocop', '~>0.81.0'
+gem 'simple_form', '~> 5.0', '>= 5.0.2'
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  include PostsHelper
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
