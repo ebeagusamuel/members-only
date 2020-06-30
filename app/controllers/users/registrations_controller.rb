0cead552f9faef0d5e0a029b5c76b2cbe76477cc
teekaytech <taofeek.olalere@feghas.com>
2020-06-30 12:30:04 +0100

    Add styling to user profile update page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,4 +62,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
+
+  def account_update_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
+  end
 end
